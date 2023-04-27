5117db6f70b10e42e4fdf0c1e5458908a2bec907
Felipe Takasawa Paiva <87485467+FTPaiva@users.noreply.github.com>
2023-04-27 20:28:04 -0300

    Add files via upload

--- Sockets/consumer2.cpp
+++ Sockets/consumer2.cpp
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <cstring>
 
+
 bool isPrime(int n) {
     if (n <= 1) {
         return false;
@@ -16,76 +17,77 @@ bool isPrime(int n) {
     return true;
 }
 
+
 int main() {
+
+    // Declara variáveis
     int server_fd, new_socket;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
 
-    // Create socket file descriptor
-    // AF_INET specifies the IPv4 Internet protocol as the communication domain for the socket
-    // SOCK_STREAM provides a reliable, sequenced, and full-duplex byte stream between the two endpoints (TCP)
+    // Cria descritor de arquivo do socket
+    // AF_INET especifica o protocolo de internet IPv4 como o domínio de comunicação para o soquete
+    // SOCK_STREAM fornece um fluxo de bytes confiável, sequencial e full-duplex entre os dois pontos finais (TCP)
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // Set socket options
-    // SOL_SOCKET is used to indicate that the option applies to the socket itself rather than to a specific protocol.
-    // SO_REUSEADDR allows a socket to bind to a port that is still in use by another socket that is in the TIME_WAIT state.
+    // Configura o socket
+    // SOL_SOCKET é usado para indicar que a opção se aplica ao próprio soquete em vez de a um protocolo específico.
+    // SO_REUSEADDR permite que um soquete se ligue a uma porta que ainda está em uso por outro soquete que está no estado TIME_WAIT.
     int opt = 1;
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        perror("setsockopt");
         exit(EXIT_FAILURE);
     }
 
-    // Bind socket to port
-    // INADDR_ANY is a special constant that represents the "any" address.
-    // It means that the server socket will be bound to all available network interfaces on the machine
+    // Associa socket à porta
+    // INADDR_ANY faz com que o socket seja ligado a todas as interfaces locais disponível
+    // AF_INET indica que o endereço será do tipo IPv4
+    // Porta 8080 escolhida arbitrariamente
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(8080);
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
-        perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
-    // Listen for incoming connections
+    // Escuta novas conexões
     // Blocking wait
-    // Max of 3 connections that can be queued up to be accepted
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
+    // Máximo de 5 conexões na fila
+    if (listen(server_fd, 5) < 0) {
         exit(EXIT_FAILURE);
     }
 
-    // Accept incoming connection
-    // new_socket = client socket descriptor
+    // Aceita conexão do cliente
+    // new_socket = descritor do socket do cliente
     if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
-        perror("accept");
         exit(EXIT_FAILURE);
     }
 
     while(1){
 
-        // Receive integer from client
+        // Recebe números inteiro do cliente
         int number;
         read(new_socket, &number, sizeof(number));
-        std::cout << "Received number: " << number << std::endl;
+        std::cout << "Número recebido: " << number << std::endl;
 
-        // Testa se numero recebido eh um zero
+        // Testa se número recebido é um zero
         if (number == 0) {
             return 0;
         }
         
-        // Check if number is prime
+        // Testa se número é primo
         bool prime = isPrime(number);
 
-        // Send result back to client
-        char result[100];
+        // Envia resultado ao cliente
+        char result[200];
+
         if (prime) {
-            strcpy(result, "The number is prime.");
+            strcpy(result, "O número é primo...");
         } else {
-            strcpy(result, "The number is not prime.");
+            strcpy(result, "O num não é primo...");
         }
+        result[strlen(result) - 1] = '\0';
         send(new_socket, result, strlen(result), 0);
     }
     return 0;
--- Sockets/productor2.cpp
+++ Sockets/productor2.cpp
@@ -4,58 +4,69 @@
 #include <unistd.h>
 #include <cstring>
 
+
 int main(int argc, char* argv[]) {
+
+    // Verifica o número de argumentos de entrada
     if (argc < 2) {
-        std::cout << "Usage: productor <number of integers to send>" << std::endl;
+        std::cout << "Por favor, passe como parâmetro apenas o número de inteiros que serão enviados." << std::endl;
         return -1;
     }
-    int quantidade_num = atoi(argv[1]);
 
+    // Obtém argumentos de entrada
+    int quantity = atoi(argv[1]);
+
+    // Declara variáveis
     int client_fd;
     struct sockaddr_in address;
 
-    // Create socket file descriptor
+    // Cria descritor de arquivo do socket
+    // AF_INET especifica o protocolo de internet IPv4 como o domínio de comunicação para o soquete
+    // SOCK_STREAM fornece um fluxo de bytes confiável, sequencial e full-duplex entre os dois pontos finais (TCP)
     if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        std::cout << "Socket creation error" << std::endl;
+        std::cout << "Erro ao criar socket" << std::endl;
         return -1;
     }
 
-    // Set server IP address and port
+    // Define o endereço IP e a porta do servidor.
     memset(&address, '0', sizeof(address));
     address.sin_family = AF_INET;
     address.sin_port = htons(8080);
     if (inet_pton(AF_INET, "127.0.0.1", &address.sin_addr) <= 0) {
-        std::cout << "Invalid address/ Address not supported" << std::endl;
+        std::cout << "Endereço inválido" << std::endl;
         return -1;
     }
 
-    // Connect to server
+    // Conecta ao servidor
     if (connect(client_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
         std::cout << "Connection Failed" << std::endl;
         return -1;
     }
 
-    
+    // Prepara ambiente para gerar números aleatórios
     int number = 1;
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dist(1, 100);
-    for (int i = 0; i < quantidade_num; i++) {
 
-        // Send  number to server
+    // Gera "quantity" números, sendo o primeiro sempre igual a um
+    for (int i = 0; i < quantity; i++) {
+
+        // Envia número ao servidor
         send(client_fd, &number, sizeof(number), 0);
-        std::cout << "Sent number: " << number << std::endl;
+        std::cout << "Número enviado: " << number << std::endl;
 
-        // Receive result from server
-        char result[100];
-        read(client_fd, result, 100);
-        std::cout << "Result: " << result << std::endl;
+        // Recebe resultado do servidor
+        char result[200];
+        read(client_fd, result, 200);
+        result[strlen(result) - 1] = '\0';
+        std::cout << "Resultado: " << result << std::endl;
 
-        // Add a delta to the number
+        // Adiciona um valor aleatório entre 1 e 100 ao número anteriror
         number += dist(gen);
     }
 
-    // Send final 0 to server
+    // Envia zero ao servidor
     int final_num = 0;
     send(client_fd, &final_num, sizeof(final_num), 0);
     std::cout << "Sent final number: " << final_num << std::endl;
